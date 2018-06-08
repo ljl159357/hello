41aa1e43857953a5ba633f171d752a7c41421058
ljl159357 <40022224+ljl159357@users.noreply.github.com>
2018-06-08 16:24:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,14 @@ if __name__ == '__main__':
     w_d = os.path.dirname(os.path.abspath(__file__))
     print('change wording dir to [{0}]'.format(w_d))
     os.chdir(w_d)
+    
+    print('################    train    ################')
+    p = os.popen(train_cmd)
+    for l in p:
+        print(l.strip())
 
-    for i in range(30):
-        
-        print('################    train    ################')
-        p = os.popen(train_cmd)
-        for l in p:
-            print(p.strip())
-
-        # eval
-        print('################    eval    ################')
-        p = os.popen(eval_cmd)
-        for l in p:
-            print(p.strip())
+    # eval
+    print('################    eval    ################')
+    p = os.popen(eval_cmd)
+    for l in p:
+        print(l.strip())
